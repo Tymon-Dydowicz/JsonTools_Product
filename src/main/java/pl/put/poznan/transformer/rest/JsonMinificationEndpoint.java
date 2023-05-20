a7009b05a106b3698778d41101e6756fd7d2a8aa
Ignacy Stępka <ignacy.stepka@gmail.com>
2023-05-20 15:51:55 +0200

    Add minification endpoint

--- src/main/java/pl/put/poznan/transformer/rest/JsonMinificationEndpoint.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonMinificationEndpoint.java
@@ -7,15 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.TextTransformer;
-
-import java.util.Arrays;
 
 @RestController
 @RequestMapping("/minification")
-public class JsonMinify {
+public class JsonMinificationEndpoint {
 
-    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
+    private static final Logger logger = LoggerFactory.getLogger(MainEndpoint.class);
 
     @RequestMapping(value = "/minify/{text}", method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json")
     public ResponseEntity<String> handleMinifyRequest(@PathVariable String text) {
