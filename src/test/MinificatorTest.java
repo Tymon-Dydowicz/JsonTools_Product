396d53c8dff212c206470ee921cafbedef210ab5
malgier148264 <97889894+shrexlover@users.noreply.github.com>
2023-05-21 21:41:33 +0200

    Add files via upload

---
+++ src/test/MinificatorTest.java
@@ -0,0 +1,44 @@
+package pl.put.poznan.transformer.logic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinificatorTest {
+    private Minificator minifi;
+
+    @BeforeEach
+    public void setup() {
+        minifi = new Minificator();
+    }
+
+    @Test
+    public void testMinifyJSON_stringNotPresent(){
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            minifi.minifyJson(null);
+        });
+    }
+
+    @Test
+    public void testMinifyJSON_wrongInputFormat(){
+        ;
+        String json = "{\"name\": \"John\", \"age\": 30, \"address\": {\"city\": \"New York\"}";
+        Assertions.assertThrows(JsonProcessingException.class, () -> {
+            minifi.minifyJson(json);
+        });
+    }
+
+    @Test
+    public void testMinifyJSON_wrongOutput() throws JsonProcessingException {
+        String json = "{\"name\": \"John\", \"age\": 30, \"city\": \"New York\"}";
+        String expectedJson = "{\"name\":\"John\",\"age\":30,\"city\":\"New York\"}";
+
+        String resultJson = minifi.minifyJson(json);
+        Assertions.assertEquals(expectedJson, resultJson);
+    }
+
+}
