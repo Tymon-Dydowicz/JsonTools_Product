308460b6b208a74fdce1e204dc6bad3d9c2c263d
Wojciech Cieśla <wojciech.tesla@gmail.com>
2023-06-12 10:53:46 +0200

    ADD: Mock testing of Minificator and XML handlers

---
+++ src/main/java/pl/put/poznan/transformer/inject/XmlToJsonImporter.java
@@ -0,0 +1,43 @@
+package pl.put.poznan.transformer.inject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.IOException;
+
+/**
+ * This class is responsible for importing XML data to JSON format using the Jackson library.
+ *
+ * @version 1.0
+ * @since 4.0
+ */
+public class XmlToJsonImporter {
+    private final ObjectMapper objectMapper;
+    private final XmlMapper xmlMapper;
+
+    public XmlToJsonImporter(ObjectMapper objectMapper, XmlMapper xmlMapper) {
+        this.objectMapper = objectMapper;
+        this.xmlMapper = xmlMapper;
+        this.xmlMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    }
+
+    /**
+     * Imports the XML data to a JSON string.
+     *
+     * @param xml the XML data as a string
+     * @return the JSON representation of the XML data
+     * @throws IOException if an I/O error occurs during the import process
+     */
+    public String importToJson(String xml) throws IOException {
+        if (xml == null) {
+            throw new IllegalArgumentException("XML string cannot be null");
+        }
+
+        JsonNode rootNode = xmlMapper.readTree(xml);
+        ObjectWriter writer = objectMapper.writer();
+        return writer.writeValueAsString(rootNode);
+    }
+}
